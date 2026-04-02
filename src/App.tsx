72549c0a48d36bce27495730b55d85c69e8d2d9d
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-04-02 18:27:17 +0100

    feat: install tanstack/react-query and implement initial App component structure with weather cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,57 @@ const App = () => {
   return (
     <>
       <Header />
+
       <div className="weather-cards">
-      <WeatherCard
-        city="Lagos"
-        country="Nigeria"
-        temperature={28}
-        condition="Sunny"
-        high={30}
-        low={26}
-        timezone="GMT +1"
-        localTime="19:19"
-        backgroundImage="/images/lagos-skyline.jpg"
-      />
-      <WeatherCard
-        city="Lagos"
-        country="Nigeria"
-        temperature={28}
-        condition="Sunny"
-        high={30}
-        low={26}
-        timezone="GMT +1"
-        localTime="19:19"
-        backgroundImage="/images/lagos-skyline.jpg"
-      />
-      <WeatherCard
-        city="Lagos"
-        country="Nigeria"
-        temperature={28}
-        condition="Sunny"
-        high={30}
-        low={26}
-        timezone="GMT +1"
-        localTime="19:19"
-        backgroundImage="/images/lagos-skyline.jpg"
-      />
+        <WeatherCard
+          city="Lagos"
+          country="Nigeria"
+          temperature={28}
+          condition="Sunny"
+          high={30}
+          low={26}
+          timezone="GMT +1"
+          localTime="19:19"
+          backgroundImage="/images/lagos-skyline.jpg"
+        />
+        <WeatherCard
+          city="Lagos"
+          country="Nigeria"
+          temperature={28}
+          condition="Sunny"
+          high={30}
+          low={26}
+          timezone="GMT +1"
+          localTime="19:19"
+          backgroundImage="/images/lagos-skyline.jpg"
+        />
+        <WeatherCard
+          city="Lagos"
+          country="Nigeria"
+          temperature={28}
+          condition="Sunny"
+          high={30}
+          low={26}
+          timezone="GMT +1"
+          localTime="19:19"
+          backgroundImage="/images/lagos-skyline.jpg"
+        />
+        <WeatherCard
+          city="Lagos"
+          country="Nigeria"
+          temperature={28}
+          condition="Sunny"
+          high={30}
+          low={26}
+          timezone="GMT +1"
+          localTime="19:19"
+          backgroundImage="/images/lagos-skyline.jpg"
+        />
       </div>
+
+
+
+      
     </>
     
   )
